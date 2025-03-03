cb6cd55e96f26d45f7ee024c85464aaf3fc6f41d
Asingura Philip <asinguraphilip@gmail.com>
2025-03-03 12:52:39 +0300

    square function

--- my-react-journey/tic-tac-toe/src/App.js
+++ my-react-journey/tic-tac-toe/src/App.js
@@ -1,12 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from 'react'
+import "./App.css";
+//COMPONENT
+function Square({value}) {
+  const [name, setValue] = useState(null);
 
+
+  function handleButtonClick() {
+    setValue('X');
+    console.log('click action', name);
+  }
+  return<button onClick={handleButtonClick} className="square">{name}</button>
+ 
+}
 
 function App() {
-  return ( 
-    <>
-    <p>stuff</p>
-    </>
+  return (
+    <div>
+      <div className="board-row">
+        <Square value={1}/>
+        <Square value={2}/>
+        <Square value={3}/>
+      </div>
+      <div className="board-row">
+        <Square/>
+        <Square/>
+        <Square/>
+      </div>
+      <div className="board-row">
+        <Square/>
+        <Square/>
+        <Square/>
+      </div>
+    </div>
   );
 }
 
